8e662622e84112cd7b8851fc79d98bd4d7b5ef14
Pranil lama <juniyalama50@gmail.com>
2026-09-10 13:51:46 -0500

    Allow hosted backend in CSP

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
               "default-src 'self'",
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:",
               "worker-src 'self' blob:",
-              "connect-src 'self' https://*.cartocdn.com https://*.basemaps.cartocdn.com https://basemaps.cartocdn.com https://services.arcgisonline.com http://localhost:8000",
+              "connect-src 'self' https://*.cartocdn.com https://*.basemaps.cartocdn.com https://basemaps.cartocdn.com https://services.arcgisonline.com http://localhost:8000 https://saving-water.onrender.com",
               "img-src 'self' data: blob: https://*.cartocdn.com https://*.basemaps.cartocdn.com https://services.arcgisonline.com https://maps.googleapis.com",
               "style-src 'self' 'unsafe-inline' https://*.cartocdn.com https://basemaps.cartocdn.com",
               "font-src 'self' data:",
